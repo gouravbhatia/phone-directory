62e969f927486af4b59a3695834615ccd6495679
Gourav Bhatia <gourav.bhatia639@gmail.com>
2019-12-04 18:02:51 +0530

    Components: Created 'Header' Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
+import Header from './Header.js';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <div className="header">
-          Phone Directory
-        </div>
+      <Header/>
         <button>ADD</button>
         <div>
           <span>NAME</span><br />
